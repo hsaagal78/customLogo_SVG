5dc5e9dbaf9a11bb5810074a5b6aa0676c8e28c1
hsagal78 <hsagal78@gmail.com>
2023-06-27 19:55:07 -0400

    second try

--- index.js
+++ index.js
@@ -4,3 +4,58 @@ const fs = require('fs');
 const { v4 } = require ('uuid');
 const shapes = require ('./shapes');
 const databaseShape = require('./models/databaseShape');
+
+function showChoices() 
+{
+    inquirer.prompt 
+    (
+        
+        {
+            type: 'input',
+            name: 'userName',
+            message: 'Enter your Name:'
+        },
+        {
+            type: 'input',
+            name: 'threeCharacters',
+            message: 'enter up to three characters'
+        },
+        {
+            type: 'input',
+            name: 'colorText',
+            message: ' enter up text color'
+        },
+        {
+            type: 'input',
+            name: 'colorShape',
+            message: 'enter up Shape color'
+        },
+        {
+            type: 'list',
+            message: 'Choose the shape',
+            name: 'welcome_option',
+            choices: ['circle', 'triangle', 'square']
+        }  
+    )
+            .then(answer => 
+                {
+                    if(answer.welcome_option === 'Exit the app')
+                    {
+                        console.log('closing the app...');
+                        process.exit();
+                    }else 
+                    {
+                        createLogo();
+                    }
+
+                }) 
+                    .catch(error => 
+                        {
+                            console.erro(error);
+                        });   
+}
+
+
+
+
+
