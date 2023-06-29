1e5d6909927cd9f5ecff3d5a393692b6d99330c5
hsagal78 <hsagal78@gmail.com>
2023-06-28 21:40:01 -0400

    make chages, options for 3 characters and capital letters or not

--- index.js
+++ index.js
@@ -6,54 +6,119 @@ const {ShapeLogo,Triangle,Circle, Rectangle} = require('./lib/shapes');
 const { describe, it, expect } = require('jest');
 const databaseShape = require('./models/dataBaseShapes');
 const logo = require('./logo.svg');
+// const { text } = require('stream/consumers');
+let text ='';
 
 
 function showChoices() {
-    inquirer
-      .prompt([
-        {
-          type: 'input',
-          name: 'userName',
-          message: 'Enter your Name:',
-        },
-        {
-          type: 'input',
-          name: 'threeCharacters',
-          message: 'Enter up to [3] characters',
-        },
-        {
-          type: 'input',
-          name: 'colorText',
-          message: 'Enter text color',
-        },
-        {
-          type: 'input',
-          name: 'colorShape',
-          message: 'Enter Shape color',
-        },
-        {
-          type: 'list',
-          message: 'Choose the shape',
-          name: 'shapeOption',
-          choices: ['Circle', 'Triangle', 'Rectangle'],
-        },
-      ])
-      .then((answer) => {
-        const { userName, threeCharacters, colorText, colorShape, shapeOption } = answer;
-  
-        if (shapeOption === 'Exit the app') {
-          console.log('Closing the app...');
-          process.exit();
-        } else {
-          createLogo(userName, threeCharacters, colorText, colorShape, shapeOption);
-        }
-      })
-      .catch((error) => {
-        console.error(error);
-      });
+          
+  // function randomCharacters() {
+      inquirer
+        .prompt([
+          {
+            type: 'input',
+            name: 'userName',
+            message: 'Enter your Name:',
+          },
+        ])
+        .then((answer) => {
+          const { userName } = answer;
+          const splitName = userName.split('');
+          const combinations = [];
+    
+          for (let i = 0; i < 4; i++) {
+            const randomPositions = [];
+            for (let j = 0; j < 3; j++) {
+              const randomPosition = Math.floor(Math.random() * splitName.length);
+              randomPositions.push(randomPosition);
+            }
+    
+            const combination = randomPositions.map((position) => splitName[position]).join('');
+            combinations.push(combination);
+          }
+    
+          console.log(combinations);
+          showCapitalLetters()
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+
+    function showCapitalLetters() {
+      inquirer
+        .prompt([
+         
+          {
+            type: 'input',
+            name: 'threeCharacters',
+            message: 'There are some example base on your name Enter up to [3] characters for your logo',
+          },
+          {
+            type: 'confirm',
+            name: 'capitalLetters',
+            message: 'Do you want Capital letter for your logo?',
+          },
+        ])
+        .then((answer) => {
+          const {threeCharacters, capitalLetters } = answer;
+    
+          if (capitalLetters) {
+           const finalThreeCharacters = threeCharacters.toUpperCase();
+           text = finalThreeCharacters;
+           console.log(finalThreeCharacters);
+           shapeAndColor();
+          }else {
+            const {threeCharacters, capitalLetters } = answer;
+            text = threeCharacters;
+            shapeAndColor();
+          }
+          
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    }
+
+  function shapeAndColor(){  
+  inquirer
+    .prompt([
+     
+   
+      {
+        type: 'input',
+        name: 'colorText',
+        message: 'Enter text color',
+      },
+      {
+        type: 'input',
+        name: 'colorShape',
+        message: 'Enter Shape color',
+      },
+      {
+        type: 'list',
+        message: 'Choose the shape',
+        name: 'shapeOption',
+        choices: ['Circle', 'Triangle', 'Rectangle'],
+      },
+    ])
+    .then((answer) => {
+      const {colorText, colorShape, shapeOption } = answer;
+
+      if (shapeOption === 'Exit the app') {
+        console.log('Closing the app...');
+        process.exit();
+      } else {
+        createLogo(colorText, colorShape, shapeOption);
+         
+      }
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+
   }
   
-  function createLogo(userName, threeCharacters, colorText, colorShape, shapeOption) {
+  function createLogo( colorText, colorShape, shapeOption) {
     const shapeClasses = {
       Circle,
       Triangle,
@@ -69,7 +134,7 @@ function showChoices() {
       <svg  
         xmlns="http://www.w3.org/2000/svg" width="300" height="200">
         ${shapeSvg}
-        <text x="150" y="100" font-size="48" fill="${colorText}" text-anchor="middle" dominant-baseline="middle">${threeCharacters}</text>
+        <text x="150" y="100" font-size="48" fill="${colorText}" text-anchor="middle" dominant-baseline="middle">${text}</text>
       </svg>
     `;
   
@@ -80,6 +145,7 @@ function showChoices() {
       }
       console.log('Generated logo.svg');
     });
-  }
+ }
+}
   
   showChoices();
